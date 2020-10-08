b8403f6f347e6135d708924104624994f113b21f
AliceAAtalla <aliceatalla36@gmail.com>
2020-10-08 19:18:43 -0300

    Updates

---
+++ precache-manifest.32a3d12fcd72e2c81978fa2c4fdd1d7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b54393e64a223ccab72c7f44dd04f6f",
+    "url": "/RecipeApp_ReactJS/index.html"
+  },
+  {
+    "revision": "3b8c736d6a991a632d8b",
+    "url": "/RecipeApp_ReactJS/static/css/main.8df12d82.chunk.css"
+  },
+  {
+    "revision": "3d439469c44b96ff3e18",
+    "url": "/RecipeApp_ReactJS/static/js/2.587e033c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RecipeApp_ReactJS/static/js/2.587e033c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8c736d6a991a632d8b",
+    "url": "/RecipeApp_ReactJS/static/js/main.c303af99.chunk.js"
+  },
+  {
+    "revision": "5bfeb3cfc378edf8dbd8",
+    "url": "/RecipeApp_ReactJS/static/js/runtime-main.90f47273.js"
+  }
+]);
